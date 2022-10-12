46eb4eb911e03dfb0c917b85ce798db55db0842b
Mike_Schaid <68658862+Mschaid@users.noreply.github.com>
2022-10-12 14:52:21 -0500

    environment

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='src'.
-    packages=find_packages()
+    name='src',
+    packages=find_packages(),
 )
